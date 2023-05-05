ce4e442cb9e510dd75afe16f98f9582c2498bfc2
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-05 15:30:32 +0100

    terraform

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "main" {
+  count = length(var.cidr_block)
+  vpc_id = var.vpc_id
+  cidr_block = var.cidr_block[count.index]
+  availability_zone = var.availability_zone[count.index]
+  tags= merge(
+    local.common_tags,
+    { Name = "${var.env}-public-subnet-${count.index+1}"
+  )
+}
