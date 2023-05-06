64d9f8c306427d93f6a15926cb46443372ab823a
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-06 13:11:25 +0100

    terraform

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_route_table" "route_table" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block        = data.aws_vpc.default.cidr_block
+    cidr_block                = data.aws_vpc.default.cidr_block
     vpc_peering_connection_id = var.vpc_peering_connection_id
   }
 
@@ -24,7 +24,15 @@ resource "aws_route_table" "route_table" {
 }
 
 resource "aws_route_table_association" "association" {
-  count = length(aws_subnet.main)
+  count          = length(aws_subnet.main)
   subnet_id      = aws_subnet.main.*.id[count.index]
   route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route" "internet_gateway" {
+  count                     = var.internet_gw ? 1 : 0
+  route_table_id            = aws_route_table.route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = var.internet_gw_id
+
 }
